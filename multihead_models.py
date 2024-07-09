288be84eb89750a394b636e3ebc8ad83cc0a58f7
soCromp <snc40@pitt.edu>
2024-07-08 23:26:36 -0500

    train/gen 1st col only

--- multihead_models.py
+++ multihead_models.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
 from collections import OrderedDict
+import torch
 from torch import nn
 from transformers.models.gpt2.modeling_gpt2 import GPT2LMHeadModel, GPT2Model
 from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaModel, LlamaForCausalLM
 from transformers.modeling_outputs import CausalLMOutputWithPast
+from torch.nn import CrossEntropyLoss
 
 SUPERCLASS_FOR_HEADLESS_LM = {GPT2LMHeadModel:GPT2Model, LlamaForCausalLM:LlamaModel}
 
@@ -15,7 +17,7 @@ class MOEMLP(nn.Module):
         super(MOEMLP, self).__init__()
         self.col = col
         
-    def from_other(mlp, col):
+    def from_other(mlp, col, num_experts):
         moemlp = MOEMLP(col)
         mlpslist = []
         for i in range(num_experts):
@@ -25,7 +27,7 @@ class MOEMLP(nn.Module):
         
         
     def forward(self, hidden_states):
-        print(self.col.value)
+        # print(self.col.value)
         return self.mlps[self.col.value](hidden_states)
 
 
@@ -76,7 +78,7 @@ class MOEMLP(nn.Module):
 #         outputs = (hidden_states) + attn_outs[1:]
         
     
-def MOEModelForCausalLM(model):
+def MOEModelForCausalLM(model, **kwargs):
     superclassForCausalLM = type(model)
     superclassForHeadlessLM = SUPERCLASS_FOR_HEADLESS_LM[type(model)]
     
@@ -112,48 +114,68 @@ def MOEModelForCausalLM(model):
         def __init__(self):
             super(MOEModelForCausalLM, self).__init__()
             self.col = Integer()
-            self.num_experts=3
+            self.num_experts=1
             
-        def from_other(model):
+        def from_other(model, num_experts=1):
             # https://stackoverflow.com/questions/597199/converting-an-object-into-a-subclass-in-python
             moemodel = deepcopy(model)
             moemodel.__class__ = MOEModelForCausalLM
             moemodel.col = Integer()
+            moemodel.num_experts = num_experts
             
             if type(model) == GPT2LMHeadModel:
                 for i in range(len(moemodel.transformer.h)):
-                    moemodel.transformer.h[i].mlp = MOEMLP.from_other(moemodel.transformer.h[i].mlp, moemodel.col)
-                    # moemodel.moemlps.append(moemodel.transformer.h[i].mlp)
+                    moemodel.transformer.h[i].mlp = MOEMLP.from_other(
+                        moemodel.transformer.h[i].mlp, moemodel.col, moemodel.num_experts)
             elif type(model) == LlamaForCausalLM:
                 moemodel = deepcopy(model)
                 print('deep copied model')
                 moemodel.__class__ = MOEModelForCausalLM
                 for i in range(len(moemodel.model.layers)):
-                    moemodel.model.layers[i].mlp = MOEMLP.from_other(moemodel.model.layers[i].mlp, moemodel.col)
-                    # moemodel.moemlps.append(moemodel.model.layers[i].mlp)
+                    moemodel.model.layers[i].mlp = MOEMLP.from_other(
+                        moemodel.model.layers[i].mlp, moemodel.col, moemodel.num_experts)
                 print('added MOE MLPs')
             else:
                 raise NotImplementedError(f'Type {type(model)} not supported')
             return moemodel
         
         def forward(self, input_ids = None, attention_mask = None, labels = None, **kwargs):
+            # input_ids, attention_mask, labels: batch x column x tokens
+            PAD = 50256
             transformer, lm_head = self.children()
             
             loss = None
-            for i in range(self.num_experts):
-                self.col.value = i
-                transformer_outputs = transformer(input_ids, attention_mask=attention_mask, **kwargs)
-                hidden_states = transformer_outputs[0]
-                lm_logits = lm_head(hidden_states)
+            # print(input_ids, attention_mask, labels)
+            
+            prompt = deepcopy(input_ids) #bs x tokens
+            if labels is not None:
+                prompt = torch.cat([prompt, labels[:,0,:][labels[:,0,:] != PAD].unsqueeze(0)], axis=1)
+                
+            mask = torch.ones_like(prompt)
+            
+            # print('prompt, mask', prompt, mask)
+                
+            # for i in range(self.num_experts):
+            self.col.value = 0
+            transformer_outputs = transformer(prompt, attention_mask=mask, **kwargs)
+            hidden_states = transformer_outputs[0]
+            lm_logits = lm_head(hidden_states)
+            
+            # if prompt is None:
+            #     prompt = labels[0]
+            # else:
+            #     prompt = torch.cat([prompt, labels[i]])
+            
+            # print(labels.shape, lm_logits.shape)
 
             
             if labels is not None:
-                print('calc loss')
+                # print('calc loss')
                 # move labels to correct device to enable model parallelism
-                labels = labels.to(lm_logits.device)
+                # labels = labels[:,0,:].to(lm_logits.device)
                 # Shift so that tokens < n predict n
                 shift_logits = lm_logits[..., :-1, :].contiguous()
-                shift_labels = labels[..., 1:].contiguous()
+                shift_labels = prompt[..., 1:].contiguous()
                 # Flatten the tokens
                 loss_fct = CrossEntropyLoss()
                 loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
@@ -166,4 +188,4 @@ def MOEModelForCausalLM(model):
                 attentions = transformer_outputs.attentions
             )
      
-    return MOEModelForCausalLM.from_other(model)
+    return MOEModelForCausalLM.from_other(model, **kwargs)
