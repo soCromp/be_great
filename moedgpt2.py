3c75399cbaa232ca20dc9500bd6c667bfa3e367d
soCromp <snc40@pitt.edu>
2024-08-01 21:16:27 -0500

    prep to train

--- moedgpt2.py
+++ moedgpt2.py
@@ -7,23 +7,23 @@ savedir = '/mnt/data/sonia/ckpts/great/adult/moegreatdgpt2-jul29/checkpoint-2257
 if not os.path.exists(savedir):
     os.makedirs(savedir)
 
-# dataset = 'adult'
-# if dataset == 'cah':
-#     data = fetch_california_housing(as_frame=True).frame
-# elif dataset == 'adult':
-#     data = pd.read_csv('adult.csv')
+dataset = 'adult'
+if dataset == 'cah':
+    data = fetch_california_housing(as_frame=True).frame
+elif dataset == 'adult':
+    data = pd.read_csv('adult.csv')
 
-# model = GReaT(llm='distilgpt2', batch_size=1,  
-#               epochs=1, save_steps=3225,
-#               experiment_dir=savedir, multihead=True)
+model = GReaT(llm='distilgpt2', batch_size=1,  
+              epochs=1, save_steps=3225,
+              experiment_dir=savedir, multihead=True)
 # cols = ['age', 'workclass', 'income']
-# # cols = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race',
-# #         'sex','capital-gain','capital-loss','hours-per-week','native-country','income']
-# model.fit(data[cols])
-# model.save(savedir)
+cols = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race',
+        'sex','capital-gain','capital-loss','hours-per-week','native-country','income']
+model.fit(data[cols])
+model.save(savedir)
 
-model = GReaT.load_from_dir(savedir)
-synthetic_data = model.sample(n_samples=10, parse=False, k=1, max_length=20)
+# model = GReaT.load_from_dir(savedir)
+synthetic_data = model.sample(n_samples=100, parse=False, k=1, max_length=140)
 synthetic_data = [l[0]+'\n' for l in synthetic_data] #remove [] around batch of 1 sample
 
 with open(os.path.join(savedir, 'samples.csv'), 'w') as f:
